6c8cee9fb4d72d944c1265999e14cbb2805b947a
simranjeet singh <95042594+SimranjeetSingh-UC@users.noreply.github.com>
2024-09-09 23:23:23 +0800

    Update customFormJS1.js

--- customFormJS1.js
+++ customFormJS1.js
@@ -1,7 +1,20 @@
-//const form = document.getElementById("customForm");
-
-//form.addEventListener("submit", onFormSubmit);
-
+const form = document.getElementById("customForm");
+form.addEventListener("submit", onFormSubmit);
+	
 function onFormSubmit(event) {
-	 alert("The form has been submitted");
+	 event.preventDefault();
+	 const MAXagtCommentLabel=document.getElementById("MAXagtCommentLabel");
+	 const MAXagtComment=document.getElementById("MAXagtComment");
+	 const SubmitButton=document.getElementById("SubmitButton");
+	 
+	 //MAXagtComment.value="";
+	 SubmitButton.innerText="Submitted";
+	 SubmitButton.style.backgroundColor="Green";
+	 SubmitButton.style.color="White";
+	 SubmitButton.type="Button";
+	 MAXagtCommentLabel.innerText="ID Process comment for next Consultant"
+	 MAXagtComment.readOnly=true;
+	
+	 //alert("The form has been submitted");
+	 event.preventDefault();
 }
