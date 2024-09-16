c16e923e872dc419d613900532a4b2c4473f5b91
simranjeet singh <95042594+SimranjeetSingh-UC@users.noreply.github.com>
2024-09-16 17:34:41 +0800

    Update customFormJS1.js

--- customFormJS1.js
+++ customFormJS1.js
@@ -10,7 +10,7 @@ console.log(parentDivContent);
 if(window.opener){
   // Open a new child pop-upwindow
 	 window.onload = function() {
-     window.childPopup = window.open('', '_blank', 'width=700,height=700');
+     const childPopup = window.open('', '_blank', 'width=700,height=700');
 	 childPopup.document.write('<html>' + form.innerHTML +'</html>');
 	 //console.log(parentDivContent.innerHTML);
 	 childPopup.document.close();
